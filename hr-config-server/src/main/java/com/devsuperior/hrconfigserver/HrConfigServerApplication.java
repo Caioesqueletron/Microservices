322956d6941fcad859a10bea293ba16ca6a6f172
Caio Vinícius Fernandes Antônio <caiocaio620@gmail.com>
2022-08-23 12:06:43 -0300

    Enviando novos projetos criados

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -1,13 +1,21 @@
 package com.devsuperior.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class HrConfigServerApplication {
+@EnableConfigServer
+public class HrConfigServerApplication{
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
+
 
 }
