514785ff5189e694a6d668e30089f964ab256f93
Caio Vinícius Fernandes Antônio <caiocaio620@gmail.com>
2022-08-24 18:33:24 -0300

    Adicionando mudanças dos projetos

--- hr-user/src/main/java/com/devsuperior/hruser/resources/UserResources.java
+++ hr-user/src/main/java/com/devsuperior/hruser/resources/UserResources.java
@@ -1,4 +1,36 @@
 package com.devsuperior.hruser.resources;
 
+import com.devsuperior.hruser.entites.User;
+import com.devsuperior.hruser.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResources {
+
+    @Autowired
+    private UserRepository repository;
+
+
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<User>> findById(@PathVariable String id){
+
+
+        Optional<User> user = repository.findById(Long.parseLong(id));
+        return  ResponseEntity.ok().body(user);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findByEmail(@RequestParam String email){
+
+        User user = repository.findByEmail(email);
+        return  ResponseEntity.ok().body(user);
+    }
+
 }
+
